872640649bb79a883faa323fb3c5dd902eac00a7
TheLastHop3 <39307359+TheLastHop3@users.noreply.github.com>
2018-06-22 19:51:58 +0200

    Options
    Details User/Driver,Edit info User/Driver,Edit pw,SignUp Driver

--- Veb_Project/Veb_Project/Controllers/TaxiController.cs
+++ Veb_Project/Veb_Project/Controllers/TaxiController.cs
@@ -11,13 +11,15 @@ namespace Veb_Project.Controllers
 {
     public class TaxiController : ApiController
     {
-        //Done:Template for SignIn,Options,SignOff,SignUp
-        //TO DO: Add driver,Add car,Add car to driver,logic
+        //Done:Template for SignIn,Options,SignOff,SignUp,Details User/Driver,Edit info User/Driver,Edit pw,SignUp Driver
+        //TO DO: logic,sort
 
         private User signedIn = new User();
+        private Driver signedInD = new Driver();
+        bool userLogged = false;
         public IHttpActionResult SignIn(string username,string password,bool checkBox)
         {
-            if (!TaxiRepository.Instance.UserExists(username,password))
+            if (!TaxiRepository.Instance.UserLogin(username,password))
             {
                 User user = new User();
                 if (checkBox)
@@ -45,7 +47,7 @@ namespace Veb_Project.Controllers
             User tmp = new User();
             tmp = user;
             tmp.Role = UserRole.Customer;
-            if (!TaxiRepository.Instance.UserExists(user.Username, user.Password))
+            if (!TaxiRepository.Instance.UserExists(user.Username))
             {
                 TaxiRepository.Instance.TaxiServiceRepository.Users.Add(tmp);
                 TaxiRepository.Instance.TaxiServiceRepository.SaveChanges();
@@ -61,6 +63,110 @@ namespace Veb_Project.Controllers
 
         }
 
+        public IHttpActionResult SignUpDriver(Driver driver)
+        {
+            Driver tmp = new Driver();
+            tmp = driver;
+            tmp.Car.Driver = driver;
+            if (!TaxiRepository.Instance.DriverExists(driver.Username) && !TaxiRepository.Instance.UserExists(tmp.Username))
+            {
+                if (!TaxiRepository.Instance.CarExists(driver.Car.CarNumber))
+                {
+                    TaxiRepository.Instance.TaxiServiceRepository.Cars.Add(tmp.Car);
+                    TaxiRepository.Instance.TaxiServiceRepository.Drivers.Add(tmp);
+                    TaxiRepository.Instance.TaxiServiceRepository.SaveChanges();
+                    return NotFound();
+                }
+                else
+                {
+                   
+                    return Ok("Car");
+                }
+            }
+            else
+            {
+                return Ok("Driver");
+            }
+            
+        }
 
+        public IHttpActionResult EditDriver(Driver driver)
+        {
+            foreach (var item in TaxiRepository.Instance.TaxiServiceRepository.Drivers)
+            {
+                if(item.Username == signedInD.Username)
+                {
+                    item.Name = driver.Name;
+                    item.LastName = driver.LastName;
+                    item.JMBG = driver.JMBG;
+                    item.Email = driver.Email;
+                    item.PhoneNumber = driver.PhoneNumber;
+                    item.Sex = driver.Sex;
+                    item.Location.Latitude = driver.Location.Latitude;
+                    item.Location.Longitude = driver.Location.Longitude;
+                    item.Location.Address.Street = driver.Location.Address.Street;
+                    item.Location.Address.City = driver.Location.Address.City;
+                    item.Location.Address.Number = driver.Location.Address.Number;
+                    item.Location.Address.PostalCode = driver.Location.Address.PostalCode;
+                    item.Car.Year = driver.Car.Year;
+                    item.Car.Registration = driver.Car.Registration;
+                    item.Car.Type = driver.Car.Type;
+                    TaxiRepository.Instance.TaxiServiceRepository.SaveChanges();
+                    return Ok(item);
+                }
+            }
+            return NotFound();
+
+        }
+
+        public IHttpActionResult EditUser(User user)
+        {
+            foreach (var item in TaxiRepository.Instance.TaxiServiceRepository.Users)
+            {
+                if (item.Username == signedIn.Username)
+                {
+                    item.Name = user.Name;
+                    item.LastName = user.LastName;
+                    item.JMBG = user.JMBG;
+                    item.Email = user.Email;
+                    item.PhoneNumber = user.PhoneNumber;
+                    item.Sex = user.Sex;
+                    TaxiRepository.Instance.TaxiServiceRepository.SaveChanges();
+                    return Ok(item);
+                }
+            }
+            return NotFound();
+
+        }
+        public IHttpActionResult EditPassword(string password)
+        {
+            if (userLogged)
+            {
+                foreach (var item in TaxiRepository.Instance.TaxiServiceRepository.Users)
+                {
+                    if(item.Username == signedIn.Username)
+                    {
+                        item.Password = password;
+                        TaxiRepository.Instance.TaxiServiceRepository.SaveChanges();
+                        return Ok();
+                    }
+                        
+                }
+            }
+            else
+            {
+                foreach (var item in TaxiRepository.Instance.TaxiServiceRepository.Drivers)
+                {
+                    if (item.Username == signedInD.Username)
+                    {
+                        item.Password = password;
+                        TaxiRepository.Instance.TaxiServiceRepository.SaveChanges();
+                        return Ok();
+                    }
+                }
+            }
+
+            return NotFound();
+        }
     }
 }
--- Veb_Project/Veb_Project/Models/DBContext/TaxiRepository.cs
+++ Veb_Project/Veb_Project/Models/DBContext/TaxiRepository.cs
@@ -39,12 +39,19 @@ namespace Veb_Project.Models.DBContext
 
         public Dictionary<string, User> SignedUp { get; set; } = new Dictionary<string, User>();
 
-        public bool UserExists(string username,string password)
+        public bool UserExists(string username)
+        {
+            return (from user in TaxiServiceRepository.Users
+                    where user.Username == username
+                    select user).ToArray().Length != 0;
+            
+        }
+        public bool UserLogin(string username, string password)
         {
             return (from user in TaxiServiceRepository.Users
                     where user.Username == username && user.Password == password
                     select user).ToArray().Length != 0;
-            
+
         }
         public bool CarExists(int carNumber)
         {
@@ -53,13 +60,18 @@ namespace Veb_Project.Models.DBContext
                     select car).ToArray().Length != 0;
         }
 
-        public bool DriverExists(string username,string password)
+        public bool DriverExists(string username)
+        {
+            return (from driver in TaxiServiceRepository.Drivers
+                    where driver.Username == username
+                    select driver).ToArray().Length != 0;
+        }
+        public bool DriverLogin(string username, string password)
         {
             return (from driver in TaxiServiceRepository.Drivers
                     where driver.Username == username && driver.Password == password
                     select driver).ToArray().Length != 0;
         }
-
         public User getUser(string username)
         {
             return (from user in TaxiServiceRepository.Users
